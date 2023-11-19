07b524b1e29a31a756bbf2ad41d24c4feef2d1d4
HoziusTeo <hoza.teodor@yahoo.com>
2023-11-19 09:19:14 +0200

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+// generated with ng g environments
+
+export const environment = {
+  apiUrl:"http://localhost:8080",
+  appName: "Wicked Burger Shop",
+  appOwner: "Hoza Teodor",
+  appLogo: "https://t3.ftcdn.net/jpg/04/77/15/04/360_F_477150474_3UHZDrbYVwAqysaxKhYQ4bWxBsKasNZJ.jpg"
+};
